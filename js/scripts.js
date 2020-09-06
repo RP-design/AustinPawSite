0b2aca08bc32c4092caa2305f8c24b4b80619f33
Rupali Pawar <rupaliapawar06@gmail.com>
2020-09-05 23:03:24 -0500

    fifth commit

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,19 @@ $(".page-header .nav-link, .navbar-brand").on("click", function(e) {
   }, 600);
 });
 
+function openPlace(evt, placeName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(placeName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
